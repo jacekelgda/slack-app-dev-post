8f2eec12256decb033200d4cbe51e5af8b97b2e8
jacekelgda <jacekelgda@gmail.com>
2018-03-15 16:34:26 +0100

    log command body

--- src/api/controllers/commands.js
+++ src/api/controllers/commands.js
@@ -3,7 +3,7 @@ import express from 'express'
 const router = new express.Router()
 
 router.all('/commands', async (req, res) => {
-    console.log(req.body.command)
+    console.log(req.body)
 
     res.status(200).send('Thanks.')
 })
