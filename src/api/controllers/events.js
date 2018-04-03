2966df9327da9661dfc1d1a0c53d2f0ea2db57d8
jacekelgda <jacekelgda@gmail.com>
2018-04-03 17:14:03 +0200

    Notification to HR channel

--- src/api/controllers/events.js
+++ src/api/controllers/events.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import filterReferrals from '../middleware/referrals'
 import { extractEmail } from '../../util/string'
+import { postReferralNotification, getMessagePermalink } from '../services/slack'
 
 const router = new express.Router()
 
@@ -8,7 +9,8 @@ router.post('/events',filterReferrals, async (req, res) => {
     let status = 200
     const referralEmail = extractEmail(req.body.event.text)
     if (referralEmail) {
-        console.log(`User: <@${req.body.event.user}> referred ${referralEmail}`)
+        const permalink = await getMessagePermalink(req.body.event.thread_ts)
+        await postReferralNotification(req.body.event.user, referralEmail, permalink.permalink)
     }
     res.status(status).send(req.body.challenge)
 })
--- src/api/services/slack.js
+++ src/api/services/slack.js
@@ -3,3 +3,5 @@ import slack from 'slack'
 module.exports.postJobOpening = (channel, text) => slack.chat.postMessage({token: process.env.slack_app_token, channel, text})
 module.exports.pinPostedMessage = postMessageResponse => slack.pins.add({token: process.env.slack_app_token, channel: postMessageResponse.channel, timestamp: postMessageResponse.ts})
 module.exports.findMessageByTs = (ts, channel) => slack.channels.history({token: process.env.slack_app_token, channel, count: 1, latest: ts, inclusive: true})
+module.exports.postReferralNotification = (user, email, permalink) => slack.chat.postMessage({token: process.env.slack_app_token, channel: process.env.hr_channel, text: `User <@${user}> reffered ${email} for ${permalink}`})
+module.exports.getMessagePermalink = ts => slack.chat.getPermalink({token: process.env.slack_app_token, channel: process.env.jobs_channel, message_ts: ts})
