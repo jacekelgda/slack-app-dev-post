4f3088df9df3f278c25bc8a1148139705ae5515e
jacekelgda <jacekelgda@gmail.com>
2018-04-03 13:24:08 +0200

    Automatically pin new job opening

--- src/api/controllers/commands.js
+++ src/api/controllers/commands.js
@@ -1,11 +1,14 @@
 import express from 'express'
-import { postJobOpening } from '../services/slack'
+import { postJobOpening, pinPostedMessage } from '../services/slack'
 
 const router = new express.Router()
 
 router.post('/commands', async (req, res) => {
     let status = 200
     await postJobOpening(process.env.jobs_channel, req.body.text)
+        .then(response => {
+            pinPostedMessage(response)
+        })
         .catch(() => {
             status = 503
         })
--- src/api/services/slack.js
+++ src/api/services/slack.js
@@ -1,3 +1,4 @@
 import slack from 'slack'
 
 module.exports.postJobOpening = (channel, text) => slack.chat.postMessage({token: process.env.slack_app_token, channel, text})
+module.exports.pinPostedMessage = postMessageResponse => slack.pins.add({token: process.env.slack_app_token, channel: postMessageResponse.channel, timestamp: postMessageResponse.ts})
